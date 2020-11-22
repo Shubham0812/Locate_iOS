72eb28ea9073d4621fd19915c11b25b65dfef778
Shubham0812 <singh.shubham0812@gmail.com>
2020-11-22 19:35:06 +0530

    Initial Commit

---
+++ Locate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locate
+//
+//  Created by Shubham Singh on 22/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
