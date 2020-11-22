6b133fd07fef6e87a93d80bf5c2e27d692a6edc7
Shubham0812 <singh.shubham0812@gmail.com>
2020-11-22 19:52:06 +0530

    Initialized project

--- Locate/ViewController.swift
+++ Locate/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK:- outlets for the viewController
+
+    
+    // MARK:- variables for the viewController
+
+    
+    // MARK:- lifeCycle for the viewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
